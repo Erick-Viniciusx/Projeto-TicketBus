d198aa2e55a16e4d0b6a357d37ff9050041f6759
ErickViniciux <erickmilhomemv@gmail.com>
2024-07-22 09:12:37 -0300

    Implementation of the user settings screen

---
+++ ticket-bus/src/app/screenUser.tsx
@@ -0,0 +1,15 @@
+import { 
+    View, 
+    Text,
+    Image, 
+    StatusBar,
+    Alert
+} from "react-native"
+
+export default function ScreenUser(){
+    return (
+        <View className="flex-1 justify-center items-center bg-black p-8">
+            <StatusBar barStyle="light-content"/>
+    )
+}
+    
